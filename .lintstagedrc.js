90560b45c79dd5095c1793d72e507f3e2ff1b9a9
jeongTaekCho <cjt3591@gmail.com>
2023-11-14 13:37:37 +0900

    📦️ Chore :  lint staged 변경

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,3 @@
 module.exports = {
-    '**/*.(ts|tsx)': filenames => [
-        `npx tsc --noEmit`,
-        `npx eslint --fix ${filenames.join(' ')}`,
-        `npx prettier --write ${filenames.join(' ')}`
-    ],
-    '**/node_modules/**/*': () => null
+    'src/**/*.{ts,tsx}': ['prettier --write', 'eslint --fix', 'git add']
 };
