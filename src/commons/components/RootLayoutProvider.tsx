d3c08dcdaf98490eadfa42a206169c5e6ff36352
jeongTaekCho <cjt3591@gmail.com>
2023-11-15 18:46:27 +0900

    🎨 Style :  Theme 설정

--- src/commons/components/RootLayoutProvider.tsx
+++ src/commons/components/RootLayoutProvider.tsx
@@ -4,12 +4,13 @@ import { ReactNode } from 'react';
 import { RecoilRoot } from 'recoil';
 import GlobalStyle from '../styles/Global';
 import { ThemeProvider } from '@emotion/react';
+import { theme } from '../styles/theme';
 
 const RootLayoutProvider = ({ children }: { children: ReactNode }) => {
     return (
         <>
             <GlobalStyle />
-            <ThemeProvider theme={{}}>
+            <ThemeProvider theme={{ theme }}>
                 <RecoilRoot>{children}</RecoilRoot>
             </ThemeProvider>
         </>
---
+++ src/commons/styles/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+    colors: {
+        'main-color': '00ffff'
+    }
+};
