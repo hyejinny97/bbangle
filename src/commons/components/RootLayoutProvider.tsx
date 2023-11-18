6a90ffb209e042673c23fa2f52e0c701eccfd23a
jeongTaekCho <cjt3591@gmail.com>
2023-11-18 19:32:27 +0900

    📦️ Chore :  React-Query 셋팅

--- src/commons/components/RootLayoutProvider.tsx
+++ src/commons/components/RootLayoutProvider.tsx
@@ -5,14 +5,19 @@ import { RecoilRoot } from 'recoil';
 import GlobalStyle from '../styles/Global';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '../styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const RootLayoutProvider = ({ children }: { children: ReactNode }) => {
+    const queryClient = new QueryClient();
+
     return (
         <>
             <GlobalStyle />
-            <ThemeProvider theme={{ theme }}>
-                <RecoilRoot>{children}</RecoilRoot>
-            </ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={{ theme }}>
+                    <RecoilRoot>{children}</RecoilRoot>
+                </ThemeProvider>
+            </QueryClientProvider>
         </>
     );
 };
