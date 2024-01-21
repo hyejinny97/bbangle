ca436ebdcd7f8cd51696c7abe8286a383f42cf2f
Cho JeongTaek <cjt3591@gmail.com>
2024-01-22 00:45:28 +0900

    🔨 Modify:  불필요한 atom 제거, input 컴포넌트 타입 수정

--- src/atoms/atom.ts
+++ src/atoms/atom.ts
@@ -1,18 +1,5 @@
 import { atom } from 'recoil';
 
-export const categoryName = '상품별';
-export const itemName = '상품';
-
-export const productNameState = atom({
-    key: 'productNameState',
-    default: '상품별'
-});
-
-export const itemNameState = atom({
-    key: 'itemNameState',
-    default: '상품'
-});
-
 export const isCategoryTabState = atom({
     key: 'isCategoryTabState',
     default: true
--- src/components/commons/inputs/SearchInput/index.tsx
+++ src/components/commons/inputs/SearchInput/index.tsx
@@ -12,6 +12,7 @@ const SearchInput = ({ placeholder, onChange }: SearchInputProps) => {
             <div className="grow shrink basis-0 h-10 px-4 py-2.5 bg-slate-100 rounded-[50px] justify-start items-center gap-1.5 flex cursor-pointer">
                 <SearchIcon />
                 <input
+                    type="text"
                     className="w-full bg-slate-100 text-color-Gray400 text-sm font-medium leading-[21px] outline-none"
                     onChange={onChange}
                     placeholder={placeholder}
