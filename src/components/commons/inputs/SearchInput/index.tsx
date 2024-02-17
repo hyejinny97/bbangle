24fa09435563a25e3fdcaf83e9ce1511cf4e1d66
hyejin <yhj970102@gmail.com>
2024-02-17 18:23:17 +0900

    ✨ Feat :  SearchInput 컴포넌트에 value, onChange props 추가

--- src/components/commons/inputs/SearchInput/index.tsx
+++ src/components/commons/inputs/SearchInput/index.tsx
@@ -1,36 +1,31 @@
 'use client';
 
 import SearchIcon from '@/components/units/Home/client/Search/assets/search.svg';
-import { useState } from 'react';
 
 interface SearchInputProps {
-    value?: string;
+    value: string;
+    onChange: (_: React.ChangeEvent<HTMLInputElement>) => void;
+    onEnter: () => void;
     placeholder: string;
-    onEnter: (_: string) => void;
 }
 
-const SearchInput = ({ placeholder, onEnter }: SearchInputProps) => {
-    const [tempKeyword, setTempKeyword] = useState('');
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTempKeyword(e.target.value);
-    };
-
+const SearchInput = ({ value, onChange, onEnter, placeholder }: SearchInputProps) => {
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-            onEnter(tempKeyword);
+            onEnter();
             e.preventDefault();
         }
     };
+
     return (
         <div className="w-full py-2.5 bg-color-White">
             <div className="grow shrink basis-0 h-10 px-4 py-2.5 bg-slate-100 rounded-[50px] justify-start items-center gap-1.5 flex cursor-pointer">
                 <SearchIcon />
                 <input
                     type="text"
-                    value={tempKeyword}
+                    value={value}
                     className="w-full bg-slate-100 text-color-Gray400  text-sm font-medium outline-none"
-                    onChange={handleChange}
+                    onChange={onChange}
                     onKeyDown={handleKeyDown}
                     placeholder={placeholder}
                 />
