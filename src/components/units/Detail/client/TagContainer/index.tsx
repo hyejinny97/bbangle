0db4cef04150a70c249c527d1bab3e9a154470a8
jijiseong <jiseong5036@gmail.com>
2024-02-18 16:47:37 +0900

    🐛 Fix :  transform 변수 명 변경으로 인한 버그 수정

--- src/components/units/Detail/client/TagContainer/index.tsx
+++ src/components/units/Detail/client/TagContainer/index.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { transformIngredientTag } from '@/commons/constants/transfromTag';
+import { transformTagToKr } from '@/commons/constants/transfromTag';
 
 interface TagContainerProps {
     tag: string;
 }
 
 function TagContainer({ tag }: TagContainerProps) {
-    tag = transformIngredientTag(tag);
+    tag = transformTagToKr(tag);
     return (
         <div className="w-auto h-[21px] bg-white rounded border border-solid border-gray-200 justify-center items-center gap-1 inline-flex">
             <span className="text-neutral-500 px-1.5 py-0.5 text-[11px] font-normal ">{tag}</span>
