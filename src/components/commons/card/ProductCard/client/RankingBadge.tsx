a6fd893284ac5d45716e447e267c83619a8070d3
Wlfjd <leejiwon813@naver.com>
2024-02-19 12:39:34 +0900

    ✨ Feat : 묶음상품뱃지 위치 재선정 및 컴포넌트로 분리

--- src/components/commons/card/ProductCard/client/RankingBadge.tsx
+++ src/components/commons/card/ProductCard/client/RankingBadge.tsx
@@ -7,7 +7,7 @@ export const RankingBadge = ({ popular, ranking }: PopularRankingProps) => {
     return (
         <>
             {popular && (
-                <div className="absolute z-10 top-[6px] left-[6px] w-5 h-5 bg-white rounded-md border border-solid border-neutral-100 flex-col justify-center items-center gap-2.5 inline-flex">
+                <div className="w-5 h-5 ml-[6px] bg-white rounded-md border border-solid border-neutral-100 flex-col justify-center items-center inline-flex">
                     <div className="text-neutral-800 text-[11px] font-bold leading-[11px]">
                         {ranking}
                     </div>
--- src/components/commons/card/ProductCard/index.tsx
+++ src/components/commons/card/ProductCard/index.tsx
@@ -8,6 +8,7 @@ import { RankingBadge } from './client/RankingBadge';
 import { ProductImage } from './client/ProductImage';
 import { ProductSummary } from './client/ProductSummary';
 import { ChooseWishListModal } from './client/ChooseWishListModal';
+import { BundleBadge } from './client/BundleBadge';
 
 interface ProductCardProps {
     product: IProductType;
@@ -23,18 +24,15 @@ const ProductCard = ({ product, popular, ranking }: ProductCardProps) => {
     return (
         <>
             <Link href={`/products/${product.boardId}`} className="w-full relative">
-                <div className="absolute z-10 top-[6px] left-[6px]  h-5 bg-[#ED5F5F] rounded-md px-[4px] flex-col justify-center items-center gap-2.5 inline-flex">
-                    <div className="text-white text-[11px] font-regular leading-[11px]">
-                        묶음 상품
-                    </div>
-                </div>
-
-                <RankingBadge popular={popular} ranking={ranking} />
                 <ProductImage
                     product={product}
                     setIsModal={setIsModal}
                     setProductId={setProductId}
                 />
+                <div className="absolute z-10 top-[6px] h-5 w-full ">
+                    <RankingBadge popular={popular} ranking={ranking} />
+                    <BundleBadge />
+                </div>
                 <ProductSummary product={product} />
             </Link>
             <ChooseWishListModal isModal={isModal} setIsModal={setIsModal} productId={productId} />
