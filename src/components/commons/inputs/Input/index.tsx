362743db763b216e5b66cefe14af93e4b482b1e3
jijiseong <jiseong5036@gmail.com>
2024-02-21 22:15:57 +0900

    🔨 Modify:  필요 없는 코드 삭제

--- src/components/commons/inputs/Input/index.tsx
+++ src/components/commons/inputs/Input/index.tsx
@@ -1,10 +1,7 @@
 import { getInputStyle } from '@/commons/utils';
 import { InputHTMLAttributes } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    buttonLabel?: string;
-    buttonDisabled?: boolean;
-}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = ({ id, type, onChange, placeholder }: InputProps) => {
     return (
