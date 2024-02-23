e9ca35fc9367f3b8cb3b35f901283ce8a2cb09d5
hyejin <yhj970102@gmail.com>
2024-02-23 15:19:52 +0900

    ♻️ Refactor :  ServerMyPage 삭제 및 app/mypage 내 page 파일로 코드 이동시킴

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,20 @@
-import ServerMyPage from '@/components/units/MyPage/server/ServerMyPage';
+import Header from '@/components/commons/header/client/Header';
+import ServerUserInfo from '@/components/units/MyPage/server/ServerUserInfo';
+import ServerSignIn from '@/components/units/MyPage/server/ServerSignIn';
+import ServerMoreInfo from '@/components/units/MyPage/server/ServerMoreInfo';
+import SeparateLine from '@/components/units/MyPage/client/SeparateLine';
+
+const IsLoggedIn = false;
 
 const MyPage = () => {
-    return <ServerMyPage />;
+    return (
+        <>
+            <Header title="마이페이지" />
+            {IsLoggedIn ? <ServerUserInfo /> : <ServerSignIn />}
+            <SeparateLine />
+            <ServerMoreInfo />
+        </>
+    );
 };
 
 export default MyPage;
