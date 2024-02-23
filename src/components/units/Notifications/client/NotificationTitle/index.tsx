9abf5e013f146fff2582681efe4c78a905d1a924
hyejin <yhj970102@gmail.com>
2024-02-23 18:01:34 +0900

    ✨ Feat :  NotificationItem -> NotificationTitle로 변경, Link 삭제, isBigTitle prop 추가

---
+++ src/components/units/Notifications/client/NotificationTitle/index.tsx
@@ -0,0 +1,22 @@
+interface NotificationTitleProps {
+    title: string;
+    date: string;
+    isBigTitle?: boolean;
+}
+
+const NotificationTitle = ({ title, date, isBigTitle = false }: NotificationTitleProps) => {
+    return (
+        <div className="p-4 border-solid border-b border-color-Gray100 leading-normal">
+            <p
+                className={`mb-[2px] ${
+                    isBigTitle ? 'text-[16px]' : 'text-[14px]'
+                } text-color-Gray900 font-medium`}
+            >
+                {title}
+            </p>
+            <p className="text-[12px] text-color-Gray500 font-normal">{date}</p>
+        </div>
+    );
+};
+
+export default NotificationTitle;
