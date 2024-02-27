ea69dfb7a020116b3d99f2e150a077950867fb2c
hyejin <yhj970102@gmail.com>
2024-02-27 11:13:53 +0900

    💄 Design:  CheckBox, Radio에 cursor-pointer 스타일 추가

--- src/components/commons/checkbox/client/Checkbox.tsx
+++ src/components/commons/checkbox/client/Checkbox.tsx
@@ -35,7 +35,7 @@ const CheckBox = ({
         required={required}
         hidden
       />
-      <label htmlFor={id} className="flex text-gray-800 text-xs font-normal">
+      <label htmlFor={id} className="flex text-gray-800 text-xs font-normal cursor-pointer">
         <span className="mr-[0.5rem]">{isChecked ? <On /> : <Off />}</span>
         {children}
       </label>
