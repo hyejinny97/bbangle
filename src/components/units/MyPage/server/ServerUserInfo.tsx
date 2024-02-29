70d71bd318c935d97192f8a79f941149f5b0e053
hyejin <yhj970102@gmail.com>
2024-02-29 12:56:29 +0900

    ✨ Feat :  마이페이지 api 연결

--- src/components/units/MyPage/server/ServerUserInfo.tsx
+++ src/components/units/MyPage/server/ServerUserInfo.tsx
@@ -1,16 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { IconCharacter } from '@/components/units/MyPage/client/Icons';
+import { fetchUserProfile } from '@/components/units/MyPage/api/fetchUserProfile';
 
-const ServerUserInfo = () => {
+const ServerUserInfo = async () => {
+  const { profileImg, nickname } = await fetchUserProfile();
+
   return (
     <div className="p-4">
       <div className="flex justify-between items-center mb-4">
         <div className="flex justify-between items-center">
-          <IconCharacter />
+          {profileImg ? (
+            <Image
+              src={profileImg}
+              width={30}
+              height={30}
+              alt={`${nickname}'s profile image`}
+              className="rounded-lg"
+            />
+          ) : (
+            <IconCharacter />
+          )}
           <p className="ml-[6px] text-[18px] text-color-Gray900 font-bold tracking-tight">
-            빵그리의 오븐
+            {nickname}
           </p>
         </div>
-        <p className="text-[11px] text-color-Gray500 tracking-tight">프로필 수정</p>
+        <Link href="/mypage/update">
+          <p className="text-[11px] text-color-Gray500 tracking-tight">프로필 수정</p>
+        </Link>
       </div>
       <p className="text-[14px] text-color-Gray800">반가워요 :) 무엇을 도와드릴까요?</p>
     </div>
