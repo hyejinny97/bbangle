7523e8d1c4963bb8a9eed4390de1ac40ea3d1b1e
hyejin <yhj970102@gmail.com>
2024-03-01 16:40:32 +0900

    ♻️ Refactor : ProductAndStoreTab에서 0, 1 -> PRODUCT_IDX, STORE_IDX 변수화

--- src/components/commons/tabs/ProductAndStoreTab.tsx
+++ src/components/commons/tabs/ProductAndStoreTab.tsx
@@ -4,6 +4,8 @@ import { useRouter, usePathname } from 'next/navigation';
 import TabContainer from '@/components/commons/tabs/TabContainer';
 
 const TAB_NAMES = ['상품', '스토어'];
+const PRODUCT_IDX = 0;
+const STORE_IDX = 1;
 
 const ProductAndStoreTab = () => {
   const router = useRouter();
@@ -11,14 +13,14 @@ const ProductAndStoreTab = () => {
   const isProductsPage = pathname.split('/').at(-1) === 'products';
 
   const handleTabChange = (activeTabIdx: number) => {
-    const path = activeTabIdx === 0 ? 'products' : 'stores';
+    const path = activeTabIdx === PRODUCT_IDX ? 'products' : 'stores';
     router.push(`/${path}`);
   };
 
   return (
     <TabContainer
       names={TAB_NAMES}
-      initActiveTabIdx={isProductsPage ? 0 : 1}
+      initActiveTabIdx={isProductsPage ? PRODUCT_IDX : STORE_IDX}
       onChange={handleTabChange}
     />
   );
