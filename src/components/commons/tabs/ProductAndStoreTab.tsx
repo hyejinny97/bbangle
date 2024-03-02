078f04b5dd5673070c093da66b78152d22be17d9
hyejin <yhj970102@gmail.com>
2024-03-02 23:23:36 +0900

    ✨ Feat : ProductAndStoreTab에 productCount/storeCount props 추가 및 path 수정

--- src/components/commons/tabs/ProductAndStoreTab.tsx
+++ src/components/commons/tabs/ProductAndStoreTab.tsx
@@ -1,25 +1,44 @@
 'use client';
 
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import TabContainer from '@/components/commons/tabs/TabContainer';
 
-const TAB_NAMES = ['상품', '스토어'];
+interface ProductAndStoreTabProps {
+  productCount?: number;
+  storeCount?: number;
+}
+
+const TAB_NAMES = {
+  product: '상품',
+  store: '스토어'
+};
 const PRODUCT_IDX = 0;
 const STORE_IDX = 1;
 
-const ProductAndStoreTab = () => {
+const ProductAndStoreTab = ({ productCount, storeCount }: ProductAndStoreTabProps) => {
+  const names = [
+    TAB_NAMES.product + (typeof productCount === 'number' ? `(${productCount})` : ''),
+    TAB_NAMES.store + (typeof storeCount === 'number' ? `(${storeCount})` : '')
+  ];
+
   const router = useRouter();
   const pathname = usePathname();
-  const isProductsPage = pathname.split('/').at(-1) === 'products';
+  const searchParams = useSearchParams();
+
+  const segments = pathname.split('/');
+  const isProductsPage = segments.pop() === 'products';
+  const defaultPath = segments.join('/');
 
   const handleTabChange = (activeTabIdx: number) => {
-    const path = activeTabIdx === PRODUCT_IDX ? 'products' : 'stores';
-    router.push(`/${path}`);
+    const path = `${defaultPath}/${activeTabIdx === PRODUCT_IDX ? 'products' : 'stores'}`;
+    const queryString = searchParams.toString();
+
+    router.push(queryString ? path + '?' + queryString : path);
   };
 
   return (
     <TabContainer
-      names={TAB_NAMES}
+      names={names}
       initActiveTabIdx={isProductsPage ? PRODUCT_IDX : STORE_IDX}
       onChange={handleTabChange}
     />
