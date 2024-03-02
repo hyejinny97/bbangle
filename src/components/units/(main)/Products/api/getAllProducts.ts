52c50f78071de9427728a05a865829c39e6f672a
hyejin <yhj970102@gmail.com>
2024-03-02 23:19:38 +0900

    ♻️ Refactor :  transformFilterValueToQueryString 함수 생성 및 getAllProducts 수정

--- src/components/units/(main)/Products/api/getAllProducts.ts
+++ src/components/units/(main)/Products/api/getAllProducts.ts
@@ -1,26 +1,10 @@
 import * as API from '@/api';
 import { GetProductsQueryProps, IAllProductsType } from '@/commons/types/allProductsType';
-import { transformCategoryToEng, transformTagToEng } from '@/commons/constants/transfromTag';
+import { transformFilterValueToQueryString } from '@/commons/utils/transformFilterValueToQueryString';
 
 export const getAllProducts = async (query: GetProductsQueryProps): Promise<IAllProductsType> => {
-  const { category, tags, sort } = query;
-  const categoryQuery = category && transformCategoryToEng(category);
-  const tagsEng = tags?.map(tag => transformTagToEng(tag));
-  const tagsQuery = tagsEng?.reduce(
-    (acc, tag) => ({
-      ...acc,
-      [tag]: true
-    }),
-    {}
-  );
-
-  const queryObject = {
-    category: categoryQuery || '',
-    ...tagsQuery,
-    sort: sort || 'LATEST'
-  };
-  const queryString = new URLSearchParams(queryObject).toString();
-  const { data } = await API.get<{ data: IAllProductsType }>(`/boards?${queryString}`);
+  const queryString = transformFilterValueToQueryString(query);
+  const { data } = await API.get<IAllProductsType>(`/boards?${queryString}`);
 
   return data;
 };
