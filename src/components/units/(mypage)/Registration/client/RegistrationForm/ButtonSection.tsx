82d5dc90697ade0abdca2ed9bd2c03bd978f18d6
jijiseong <jiseong5036@gmail.com>
2024-03-02 14:51:33 +0900

    ✨ Feat :  건너뛰기 기능 추가

--- src/components/units/(mypage)/Registration/client/RegistrationForm/ButtonSection.tsx
+++ src/components/units/(mypage)/Registration/client/RegistrationForm/ButtonSection.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import Button from '@/components/commons/button/client/Button';
+import Link from 'next/link';
 
 function ButtonSection() {
   return (
     <div className="flex gap-[10px] w-full">
-      <Button variants="primary-white">건너뛰기</Button>
+      <Button type="button" variants="primary-white">
+        <Link href="/">건너뛰기</Link>
+      </Button>
       <Button type="submit" variants="primary-black">
         완료
       </Button>
