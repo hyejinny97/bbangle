70f27bbaeb180294eff6b056bc52b7ee5b39f268
jijiseong <jiseong5036@gmail.com>
2024-03-02 17:17:02 +0900

    📦️ Chore :  turbopack 적용 & 안쓰는 패키지 정리

--- next.config.js
+++ next.config.js
@@ -1,42 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'k.kakaocdn.net',
-  //       port: '443',
-  //       pathname: '/dn/**'
-  //     }
-  //   ]
-  // },
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'firebasestorage.googleapis.com'
-      },
-      { hostname: 'k.kakaocdn.net' }
-    ]
-  },
-  experimental: {
-    // appDir: true,
-    turbo: {
-      rules: {
-        // Option format
-        '*.md': [
-          {
-            loader: '@mdx-js/loader',
-            options: {
-              format: 'md'
-            }
-          }
-        ],
-        // Option-less format
-        '*.mdx': ['@mdx-js/loader']
-      },
-      resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json']
-    }
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -46,6 +9,17 @@ const nextConfig = {
 
     return config;
   },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js'
+        }
+      }
+    }
+  },
+
   images: {
     remotePatterns: [{ hostname: 'firebasestorage.googleapis.com' }, { hostname: 'k.kakaocdn.net' }]
   }
--- src/components/commons/RootLayoutProvider.tsx
+++ src/components/commons/RootLayoutProvider.tsx
@@ -2,23 +2,15 @@
 
 import { ReactNode } from 'react';
 import { RecoilRoot } from 'recoil';
-import GlobalStyle from '../../commons/styles/Global';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from '../../commons/styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const RootLayoutProvider = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
-    <>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={{ theme }}>
-          <RecoilRoot>{children}</RecoilRoot>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>{children}</RecoilRoot>
+    </QueryClientProvider>
   );
 };
 
