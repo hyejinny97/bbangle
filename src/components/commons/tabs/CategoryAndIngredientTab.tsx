36fa84d0de8b448f03eb4b2069daa19bae9f037d
hyejin <yhj970102@gmail.com>
2024-03-05 00:17:25 +0900

    🐛 Fix : TabContainer 내 useState 제거, 상위 Tab에서 activeTabIdx 조절

--- src/components/commons/tabs/CategoryAndIngredientTab.tsx
+++ src/components/commons/tabs/CategoryAndIngredientTab.tsx
@@ -10,19 +10,14 @@ const INGREDIENT_IDX = 1;
 
 const CategoryAndIngredientTab = () => {
   const [isCategoryTab, setIsCategoryTab] = useRecoilState(isCategoryTabStateNew);
+  const activeTabIdx = isCategoryTab ? CATEGORY_IDX : INGREDIENT_IDX;
 
   const handleTabChange = (activeTabIdx: number) => {
     setIsCategoryTab(activeTabIdx === CATEGORY_IDX);
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  return (
-    <TabContainer
-      names={TAB_NAMES}
-      initActiveTabIdx={isCategoryTab ? CATEGORY_IDX : INGREDIENT_IDX}
-      onChange={handleTabChange}
-    />
-  );
+  return <TabContainer names={TAB_NAMES} activeTabIdx={activeTabIdx} onChange={handleTabChange} />;
 };
 
 export default CategoryAndIngredientTab;
