b1a39d1b2036b2ff7e470e007d1b5dd394a5d7b5
hyejin <yhj970102@gmail.com>
2024-03-04 16:52:36 +0900

    ✨ Feat : 검색 상세 페이지에서 필터값에 따라 탭 숫자가 변경되지 않도록 수정

--- src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
+++ src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
@@ -1,17 +1,14 @@
 'use client';
 
-import { useRecoilValue } from 'recoil';
-import { filterValueState } from '@/atoms/atom';
 import { useSearchParams } from 'next/navigation';
 import { useGetSearchResultQuery } from '@/components/units/Search/hooks/useGetSearchResultQuery';
 import ProductAndStoreTab from '@/components/commons/tabs/ProductAndStoreTab';
 
 const ProductAndStoreTabWithCount = () => {
   const searchParams = useSearchParams();
-  const filterValue = useRecoilValue(filterValueState);
   const { data } = useGetSearchResultQuery({
     keyword: searchParams.get('query') || '',
-    filterValue
+    filterValue: { category: '', tags: [] }
   });
 
   return (
