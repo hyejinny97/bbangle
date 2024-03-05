80c1dff34010f54ed97fabf39bd6ea57bbc5a5e9
hyejin <yhj970102@gmail.com>
2024-03-05 23:00:07 +0900

    ✨ Feat : useGetSearchStoresQuery 내 useQuery -> useInfiniteQuery로 수정

--- src/components/units/Search/hooks/useGetSearchStoresQuery.ts
+++ src/components/units/Search/hooks/useGetSearchStoresQuery.ts
@@ -1,30 +1,51 @@
 import * as API from '@/api';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { IAllStoreType } from '@/commons/types/allstoreType';
 
 interface GetSearchStoresProps {
   keyword: string;
+  pageParam: number;
 }
 
-const getSearchStores = async ({ keyword }: GetSearchStoresProps): Promise<IAllStoreType> => {
-  try {
-    if (!keyword) return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0 };
+interface UseGetSearchStoresQueryProps {
+  keyword: string;
+}
 
-    const { data } = await API.get<IAllStoreType>(`/search/stores?keyword=${keyword}&page=0`);
+const getSearchStores = async ({
+  keyword,
+  pageParam
+}: GetSearchStoresProps): Promise<IAllStoreType> => {
+  try {
+    if (!keyword)
+      return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0, existNextPage: false };
+
+    const { data } = await API.get<IAllStoreType>(
+      `/search/stores?keyword=${keyword}&page=${pageParam}`
+    );
 
     return data;
   } catch (error) {
     console.error(error);
-    return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0 };
+    return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0, existNextPage: false };
   }
 };
 
-export const useGetSearchStoresQuery = ({ keyword }: GetSearchStoresProps) => {
-  return useQuery<IAllStoreType, Error>({
+export const useGetSearchStoresQuery = ({ keyword }: UseGetSearchStoresQueryProps) => {
+  const { data, ...rest } = useInfiniteQuery({
     queryKey: ['searchStores', keyword],
-    queryFn: () => getSearchStores({ keyword }),
+    queryFn: ({ pageParam }: { pageParam: number }) => getSearchStores({ keyword, pageParam }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, __, lastPageParam) => {
+      const nextPageParam = lastPage.existNextPage ? lastPageParam + 1 : undefined;
+      return nextPageParam;
+    },
     refetchOnMount: false,
     refetchOnReconnect: false,
     refetchOnWindowFocus: false
   });
+
+  const stores = data?.pages.map(page => page.content).flat();
+  const itemCount = data?.pages[0]?.itemCount || 0;
+
+  return { stores, itemCount, ...rest };
 };
