8a843df5b378c8ca6dd13ef8c0e74707156a89d3
hyejin <yhj970102@gmail.com>
2024-03-05 17:48:05 +0900

    ✨ Feat : ProductAndStoreTabWithCount에서 useGetSearchProductsQuery, useGetSearchStoresQuery 사용

--- src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
+++ src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
@@ -1,20 +1,24 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useGetSearchResultQuery } from '@/components/units/Search/hooks/useGetSearchResultQuery';
+import { useGetSearchProductsQuery } from '@/components/units/Search/hooks/useGetSearchProductsQuery';
+import { useGetSearchStoresQuery } from '@/components/units/Search/hooks/useGetSearchStoresQuery';
 import ProductAndStoreTab from '@/components/commons/tabs/ProductAndStoreTab';
 
 const ProductAndStoreTabWithCount = () => {
   const searchParams = useSearchParams();
-  const { data } = useGetSearchResultQuery({
-    keyword: searchParams.get('query') || '',
+  const keyword = searchParams.get('query') || '';
+
+  const { data: products } = useGetSearchProductsQuery({
+    keyword,
     filterValue: { category: '', tags: [] }
   });
+  const { data: stores } = useGetSearchStoresQuery({ keyword });
 
   return (
     <ProductAndStoreTab
-      productCount={data && data.products.length}
-      storeCount={data && data.stores.length}
+      productCount={products && products.itemCount}
+      storeCount={stores && stores.itemCount}
     />
   );
 };
