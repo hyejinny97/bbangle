afd002a8a930d069dc26f302d433e96b650cecdd
hyejin <yhj970102@gmail.com>
2024-03-05 23:25:08 +0900

    ♻️ Refactor : Footer 수정

--- src/components/commons/footer/server/Footer.tsx
+++ src/components/commons/footer/server/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import Button from '../client/Button';
 import Category from '@/components/commons/footer/assets/category.svg';
 import Heart from '@/components/commons/footer/assets/heart.svg';
 import Profile from '@/components/commons/footer/assets/profile.svg';
@@ -16,23 +16,23 @@ const Footer = () => {
 
   // 나중에 따로 뺄거
   const menu = [
-    { title: '홈', icon: Home, page: '/' },
-    { title: '검색', icon: Search, page: '/search' },
-    { title: '전체', icon: Category, page: '/products' },
-    { title: '찜', icon: Heart, page: '/wishlist' },
-    { title: '마이페이지', icon: Profile, page: '/mypage' }
+    { title: '홈', icon: Home, href: '/' },
+    { title: '검색', icon: Search, href: '/search' },
+    { title: '전체', icon: Category, href: '/products' },
+    { title: '찜', icon: Heart, href: '/wishlist' },
+    { title: '마이페이지', icon: Profile, href: '/mypage' }
   ];
 
   return (
     <div className="sm:w-[600px] sm:left-1/2 sm:translate-x-[-50%] py-[10px] flex justify-between fixed left-0 bottom-0 w-full bg-white border-t border-neutral-100 z-[100]">
-      {menu.map((item, index) => {
+      {menu.map(item => {
         const isHomePage = pathname === '/';
-        const isHomeLink = item.page === '/';
+        const isHomeLink = item.href === '/';
 
         const isStorePage = pathname === '/stores';
-        const isProductLink = item.page === '/products';
+        const isProductLink = item.href === '/products';
 
-        let isActive = pathname.startsWith(item.page);
+        let isActive = pathname.startsWith(item.href);
         if (!isHomePage && isHomeLink) {
           isActive = false;
         }
@@ -41,13 +41,18 @@ const Footer = () => {
         }
 
         return (
-          <Button
-            key={index}
-            title={item.title}
-            icon={<item.icon stroke={isActive ? PRIMARY_ORANGE_RED : GRAY_500} />}
-            page={item.page}
-            isActive={isActive}
-          />
+          <Link
+            key={item.title}
+            href={item.href}
+            className="flex flex-col items-center justify-center w-1/5 gap-2 cursor-pointer"
+          >
+            <item.icon stroke={isActive ? PRIMARY_ORANGE_RED : GRAY_500} />
+            <span
+              className={`text-neutral-400 text-xs font-normal leading-[18px] ${isActive ? 'text-primaryOrangeRed' : 'text-gray-500'}`}
+            >
+              {item.title}
+            </span>
+          </Link>
         );
       })}
     </div>
