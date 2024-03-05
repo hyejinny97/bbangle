5de215af60ca5adf3b7adcb89787756821965b3f
hyejin <yhj970102@gmail.com>
2024-03-05 22:58:41 +0900

    ✨ Feat : useGetSearchProductsQuery 내 useQuery -> useInfiniteQuery로 수정

--- src/components/units/Search/hooks/useGetSearchProductsQuery.ts
+++ src/components/units/Search/hooks/useGetSearchProductsQuery.ts
@@ -1,38 +1,60 @@
 import * as API from '@/api';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { GetProductsQueryProps, IAllProductsType } from '@/commons/types/allProductsType';
 import { transformFilterValueToQueryString } from '@/commons/utils/transformFilterValueToQueryString';
 
 interface GetSearchProductsProps {
   keyword: string;
   filterValue: GetProductsQueryProps;
+  pageParam: number;
+}
+
+interface UseGetSearchProductsQueryProps {
+  keyword: string;
+  filterValue: GetProductsQueryProps;
 }
 
 const getSearchProducts = async ({
   keyword,
-  filterValue
+  filterValue,
+  pageParam
 }: GetSearchProductsProps): Promise<IAllProductsType> => {
   try {
-    if (!keyword) return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0 };
+    if (!keyword)
+      return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0, existNextPage: false };
 
     const queryString = transformFilterValueToQueryString(filterValue);
     const { data } = await API.get<IAllProductsType>(
-      `/search/boards?keyword=${keyword}&${queryString}&page=0`
+      `/search/boards?keyword=${keyword}&${queryString}&page=${pageParam}`
     );
 
     return data;
   } catch (error) {
     console.error(error);
-    return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0 };
+    return { content: [], itemCount: 0, pageNumber: 0, pageSize: 0, existNextPage: false };
   }
 };
 
-export const useGetSearchProductsQuery = ({ keyword, filterValue }: GetSearchProductsProps) => {
-  return useQuery<IAllProductsType, Error>({
+export const useGetSearchProductsQuery = ({
+  keyword,
+  filterValue
+}: UseGetSearchProductsQueryProps) => {
+  const { data, ...rest } = useInfiniteQuery({
     queryKey: ['searchProducts', keyword, filterValue],
-    queryFn: () => getSearchProducts({ keyword, filterValue }),
+    queryFn: ({ pageParam }: { pageParam: number }) =>
+      getSearchProducts({ keyword, filterValue, pageParam }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, __, lastPageParam) => {
+      const nextPageParam = lastPage.existNextPage ? lastPageParam + 1 : undefined;
+      return nextPageParam;
+    },
     refetchOnMount: false,
     refetchOnReconnect: false,
     refetchOnWindowFocus: false
   });
+
+  const products = data?.pages.map(page => page.content).flat();
+  const itemCount = data?.pages[0]?.itemCount || 0;
+
+  return { products, itemCount, ...rest };
 };
