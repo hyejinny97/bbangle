5d18ce85168e7e32b0f64c5a16ffb250f129bbab
hyejin <yhj970102@gmail.com>
2024-03-06 16:46:25 +0900

    ✨ Feat : 전체 페이지(상품) 무한 스크롤 구현

--- src/components/units/(main)/Products/hooks/useGetAllProductsQuery.ts
+++ src/components/units/(main)/Products/hooks/useGetAllProductsQuery.ts
@@ -1,10 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
-import { GetProductsQueryProps, IAllProductsType } from '@/commons/types/allProductsType';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { ProductsQueryType } from '@/commons/types/allProductsType';
 import { getAllProducts } from '../api/getAllProducts';
 
-export const useGetAllProductsQuery = (query: GetProductsQueryProps) => {
-  return useQuery<IAllProductsType, Error>({
-    queryKey: ['products'],
-    queryFn: () => getAllProducts(query)
+export const useGetAllProductsQuery = (query: ProductsQueryType) => {
+  const { data, ...rest } = useInfiniteQuery({
+    queryKey: ['products', query],
+    queryFn: ({ pageParam }: { pageParam: number }) => getAllProducts({ query, pageParam }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, __, lastPageParam) => {
+      const nextPageParam = lastPage.last ? undefined : lastPageParam + 1;
+      return nextPageParam;
+    },
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false
   });
+
+  const products = data?.pages.map(page => page.content).flat();
+
+  return { products, ...rest };
 };
