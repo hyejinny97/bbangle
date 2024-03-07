140371120db15fca16e8881e5b80c3be580db11c
hyejin <yhj970102@gmail.com>
2024-03-07 21:31:11 +0900

    ✨ Feat : Select, ProductSortSelect, WishListSortSelect 컴포넌트 생성 및 반영

---
+++ src/components/commons/selects/Select.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import DownBtn from '@/components/commons/selects/assets/downBtn.svg';
+
+interface SelectProps {
+  options: Array<string>;
+  selectedOption: string;
+  onChange: (_selectedOption: string) => void;
+}
+
+const Select = ({ options, selectedOption, onChange }: SelectProps) => {
+  const [isExpended, setIsExpended] = useState(false);
+
+  useEffect(() => {
+    const handleWindowClick = (e: MouseEvent) => {
+      const clickedTarget = e.target as HTMLElement;
+      if (clickedTarget.closest('.selectEl')) return;
+
+      setIsExpended(false);
+    };
+    window.addEventListener('click', handleWindowClick);
+
+    return () => window.removeEventListener('click', handleWindowClick);
+  }, []);
+
+  const handleSelectClick = () => {
+    setIsExpended(!isExpended);
+  };
+
+  return (
+    <div
+      className="selectEl relative inline-block text-gray-900 text-xs font-medium"
+      onClick={handleSelectClick}
+    >
+      <div className="flex items-center p-[8px] pl-[12px] border-solid border-[1px] border-gray-200 rounded-[50px] cursor-pointer">
+        <span className="mr-[4px]">{selectedOption}</span>
+        <span className={`${isExpended ? 'rotate-180 transition-all' : ''}`}>
+          <DownBtn />
+        </span>
+      </div>
+      {isExpended && (
+        <ul className="absolute mt-1 top-full z-[101] rounded-[10px] shadow bg-color-White">
+          {options.map((option, index) => {
+            const firstOption = index === 0;
+            const lastOption = index === options.length - 1;
+
+            const borderStyle = lastOption ? 'border-b-0' : 'border-b-[1px]';
+            const hoverRoundedStyle = firstOption
+              ? 'hover:rounded-t-[10px]'
+              : lastOption
+                ? 'hover:rounded-b-[10px]'
+                : '';
+
+            return (
+              <li
+                key={option}
+                onClick={() => onChange(option)}
+                className={`px-[16px] py-[10px] border-gray-100 cursor-pointer hover:bg-gray-50 ${borderStyle} ${hoverRoundedStyle}`}
+              >
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Select;
--- src/components/units/WishListDetail/client/WishDetail/index.tsx
+++ src/components/units/WishListDetail/client/WishDetail/index.tsx
@@ -1,14 +1,8 @@
 'use client';
 
 import ProductCard from '@/components/commons/card/ProductCard';
-import FilterSelect from '@/components/commons/selects/FilterSelect';
+import WishListSortSelect from '@/components/commons/selects/WishListSortSelect';
 import { useWishProductListQuery } from '@/components/units/WishListDetail/hooks/useWishProductsListQuery';
-
-const FILTER_DATA = [
-  { name: 'aaa', value: '담은순' },
-  { name: 'aaa', value: '인기순' },
-  { name: 'aaa', value: '저가순' }
-];
 
 interface WishDetailProps {
   folderId: number;
@@ -19,7 +13,7 @@ const WishDetail = ({ folderId }: WishDetailProps) => {
   return (
     <div className="w-[92%]">
       <div className="mb-5">
-        <FilterSelect datas={FILTER_DATA} />
+        <WishListSortSelect />
       </div>
       <div className="flex flex-wrap w-full  m-auto gap-x-[4%] gap-y-4">
         {wishProductList?.content?.map((product, i) => (
