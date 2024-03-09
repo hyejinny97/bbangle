8cdb50711bcf23f9da57ff2642c93f58afec4e7c
hyejin <yhj970102@gmail.com>
2024-03-09 19:38:27 +0900

    ✨ Feat : 문의하기 - 카카오채팅 구현

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,11 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare global {
+  export interface Window {
+    Kakao: any;
+  }
+}
+
+export {};
