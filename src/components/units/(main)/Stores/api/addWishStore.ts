eadb5f740a49400bc38faef3ac3c538ba64dd4ae
jijiseong <jiseong5036@gmail.com>
2024-03-09 16:03:19 +0900

    🐛 Fix :  addWishStore 에러 수정

--- src/components/units/(main)/Stores/api/addWishStore.ts
+++ src/components/units/(main)/Stores/api/addWishStore.ts
@@ -2,7 +2,6 @@ import API from '@/api';
 import { WishStoreListReturn } from '../../types';
 
 export const addWishStore = async (storeId: number) => {
-  const res = await API.post(`/likes/store/${String(storeId)}`);
-  const data: WishStoreListReturn = await res.json();
+  const data: WishStoreListReturn = await API.post(`/likes/store/${String(storeId)}`);
   return data;
 };
