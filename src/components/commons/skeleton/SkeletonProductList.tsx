ac717ee92899caddc8dedb66bef56471bc347696
Wlfjd <leejiwon813@naver.com>
2024-03-10 23:21:53 +0900

    🐛 Fix :  bestProducts 타입 지정

--- src/components/commons/skeleton/SkeletonProductList.tsx
+++ src/components/commons/skeleton/SkeletonProductList.tsx
@@ -3,10 +3,16 @@ import { SkeletonProductCard } from './SkeletonProductCard';
 export const SkeletonProductList = () => {
   return (
     <>
-      <div className="flex flex-wrap w-[92%] m-auto  gap-x-[4%] gap-y-4">
+      <div className="flex flex-wrap w-[92%] m-auto gap-x-[4%] pb-4">
+        <SkeletonProductCard />
         <SkeletonProductCard />
       </div>
-      <div className="flex flex-wrap w-[92%] m-auto  gap-x-[4%] gap-y-4">
+      <div className="flex flex-wrap w-[92%] m-auto  gap-x-[4%] pb-4">
+        <SkeletonProductCard />
+        <SkeletonProductCard />
+      </div>
+      <div className="flex flex-wrap w-[92%] m-auto  gap-x-[4%] pb-4">
+        <SkeletonProductCard />
         <SkeletonProductCard />
       </div>
     </>
--- src/components/units/Home/client/ProductsList/index.tsx
+++ src/components/units/Home/client/ProductsList/index.tsx
@@ -1,5 +1,6 @@
 import ProductCard from '@/components/commons/card/ProductCard';
 import API from '@/api';
+import { IProductType } from '@/commons/types/productType';
 
 const getBestProducts = async () => {
   try {
@@ -15,7 +16,7 @@ const ProductsList = async () => {
   const bestProducts = await getBestProducts();
   return (
     <div className="flex flex-wrap w-[92%] m-auto  gap-x-[4%] gap-y-4">
-      {bestProducts?.content?.map((product: any) => (
+      {bestProducts?.content?.map((product: IProductType) => (
         <div key={String(product.boardId)} className="w-[48%]">
           <ProductCard product={product} />
         </div>
