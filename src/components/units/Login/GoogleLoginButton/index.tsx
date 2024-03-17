e090c9b01b3fd5f107ffae1fdcbfe5c4ece58e59
jijiseong <jiseong5036@gmail.com>
2024-03-17 19:30:15 +0900

    feat: google login 동작 추가

--- src/components/units/Login/GoogleLoginButton/index.tsx
+++ src/components/units/Login/GoogleLoginButton/index.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import API from '@/api';
 import GoogleIcon from './assets/google_logo.svg';
 
 const GoogleLoginButton = () => {
+  const handleGoggleLogin = async () => {
+    window.location.assign(`${API.serverUrl}/oauth2/authorization/google`);
+  };
+
   return (
-    <button className="border border-gray-100 rounded-[10px] flex gap-[8px] items-center justify-center h-[52px] bg-white shadow text-black">
+    <button
+      onClick={handleGoggleLogin}
+      className="border border-gray-100 rounded-[10px] flex gap-[8px] items-center justify-center h-[52px] bg-white shadow text-black"
+    >
       <GoogleIcon />
       <div className=" text-black text-opacity-90 text-base font-medium font-['Pretendard'] leading-normal">
         구글 시작하기
