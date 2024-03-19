8d462770c9fba76a614f6a3087d8a16f6e5d19c0
hyejin <yhj970102@gmail.com>
2024-03-19 20:30:40 +0900

    💄 Design: Home page 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import ServerHome from '@/components/units/Home/server/ServerHome';
+import LogoContainer from '@/components/units/Home/client/LogoContainer';
+import Banner from '@/components/units/Home/client/Banner';
+import Search from '@/components/units/Home/client/Search';
+import ServerCategory from '@/components/units/Home/server/ServerCategory';
+import ServerProducts from '@/components/units/Home/server/ServerProducts';
 
 export default function Home() {
-  return <ServerHome />;
+  return (
+    <>
+      <LogoContainer />
+      <Search />
+      <Banner />
+      <ServerCategory />
+      <ServerProducts />
+    </>
+  );
 }
--- src/components/commons/inputs/SearchInput/index.tsx
+++ src/components/commons/inputs/SearchInput/index.tsx
@@ -2,15 +2,16 @@
 
 import SearchIcon from '@/components/units/Home/client/Search/assets/search.svg';
 
-interface SearchInputProps {
-  value: string;
-  onChange: (_: React.ChangeEvent<HTMLInputElement>) => void;
-  onEnter: () => void;
-  placeholder: string;
+interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+  onChange?: (_: React.ChangeEvent<HTMLInputElement>) => void;
+  onEnter?: () => void;
 }
 
-const SearchInput = ({ value, onChange, onEnter, placeholder }: SearchInputProps) => {
+const SearchInput = ({ value, onChange, onEnter, ...rest }: SearchInputProps) => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!onEnter) return;
+
     if (e.key === 'Enter') {
       onEnter();
       e.preventDefault();
@@ -18,18 +19,16 @@ const SearchInput = ({ value, onChange, onEnter, placeholder }: SearchInputProps
   };
 
   return (
-    <div className="w-full py-2.5 bg-color-White">
-      <div className="grow shrink basis-0 h-10 px-4 py-2.5 bg-slate-100 rounded-[50px] justify-start items-center gap-1.5 flex cursor-pointer">
-        <SearchIcon />
-        <input
-          type="text"
-          value={value}
-          className="w-full bg-slate-100 text-color-Gray400  text-sm font-medium outline-none"
-          onChange={onChange}
-          onKeyDown={handleKeyDown}
-          placeholder={placeholder}
-        />
-      </div>
+    <div className="flex items-center gap-[6px] px-[16px] py-[10px] w-full bg-blueGray-30 rounded-[50px] cursor-pointer">
+      <SearchIcon />
+      <input
+        type="text"
+        value={value}
+        className="w-full bg-blueGray-30 text-gray-400 text-14 font-medium leading-150 tracking-tight-2 outline-none"
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
+        {...rest}
+      />
     </div>
   );
 };
