fa9d8b1149d9ceb1e507cd32807d2856e850f204
hyejin <yhj970102@gmail.com>
2024-03-19 22:33:30 +0900

    ✨ Feat : 검색페이지 상품/스토어 탭 내 상품 개수가 필터값에 따라 변경되도록 수정

--- src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
+++ src/components/units/Search/client/ProductAndStoreTabWithCount/index.tsx
@@ -1,26 +1,19 @@
 'use client';
 
+import { Suspense } from 'react';
+import { useRecoilValue } from 'recoil';
 import { useSearchParams } from 'next/navigation';
+import { filterValueState } from '@/components/units/(main)/Products/atoms';
 import { useGetSearchProductsQuery } from '@/components/units/Search/hooks/useGetSearchProductsQuery';
 import { useGetSearchStoresQuery } from '@/components/units/Search/hooks/useGetSearchStoresQuery';
 import ProductAndStoreTab from '@/components/commons/tabs/ProductAndStoreTab';
-import { LIMIT_MIN_PRICE, LIMIT_MAX_PRICE } from '@/commons/constants/priceLimit';
-import { Suspense } from 'react';
 
 const ProductAndStoreTabWithCount = () => {
+  const filterValue = useRecoilValue(filterValueState);
   const searchParams = useSearchParams();
   const keyword = searchParams.get('query') || '';
 
-  const { itemCount: productCount } = useGetSearchProductsQuery({
-    keyword,
-    filterValue: {
-      category: '',
-      tags: [],
-      sort: '추천순',
-      showProductsAvailableOrder: false,
-      price: { minPrice: LIMIT_MIN_PRICE, maxPrice: LIMIT_MAX_PRICE }
-    }
-  });
+  const { itemCount: productCount } = useGetSearchProductsQuery({ keyword, filterValue });
   const { itemCount: storeCount } = useGetSearchStoresQuery({ keyword });
 
   return (
