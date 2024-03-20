b6f801eb9a92d20297d268e243648db6888486ff
hyejin <yhj970102@gmail.com>
2024-03-21 01:02:53 +0900

    ✨ Feat : `app/(main)/stores` page 내 ProductAnsStoreTab -> ProductAndStoreTabWithCount로 변경

--- src/app/(main)/stores/page.tsx
+++ src/app/(main)/stores/page.tsx
@@ -1,18 +1,13 @@
 import Header from '@/components/commons/header/client/Header';
-import ProductAndStoreTab from '@/components/commons/tabs/ProductAndStoreTab';
+import ProductAndStoreTabWithCount from '@/components/units/(main)/client/ProductAndStoreTabWithCount';
 import StoresTab from '@/components/units/(main)/Stores/client/StoresTab';
-import { Suspense } from 'react';
 
 const Stores = () => {
   return (
     <>
       <Header title="전체보기" />
-      <Suspense>
-        <ProductAndStoreTab />
-      </Suspense>
-      <div className="flex flex-wrap m-auto">
-        <StoresTab />
-      </div>
+      <ProductAndStoreTabWithCount />
+      <StoresTab />
     </>
   );
 };
