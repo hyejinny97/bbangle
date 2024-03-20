d6809bb326ff8a43fd94b366bbf60f25eda844cd
jijiseong <jiseong5036@gmail.com>
2024-03-20 17:13:54 +0900

    🔨 Modify:  kakao Script -> script 수정

--- src/components/units/(mypage)/MyPage/client/KaKaoChatScript.tsx
+++ src/components/units/(mypage)/MyPage/client/KaKaoChatScript.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import Script from 'next/script';
-
 const KaKaoChatScript = () => {
   const handleScriptLoad = () => {
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
   };
 
   return (
-    <Script
+    <script
       defer
       src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.channel.min.js"
       integrity="sha384-ty4605mXDCz/lA+RRt4bHCRa1c9uIaIi0JrsmmWVxaNJZzu58jMhJK8wAMqDxrYv"
