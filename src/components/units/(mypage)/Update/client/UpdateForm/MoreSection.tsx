4654f876dde21c12a330f82871f4432b98257bb4
jijiseong <jiseong5036@gmail.com>
2024-03-31 04:20:21 +0900

    🐛 Fix :  로그아웃 버그 수정

--- src/components/units/(mypage)/Update/client/UpdateForm/MoreSection.tsx
+++ src/components/units/(mypage)/Update/client/UpdateForm/MoreSection.tsx
@@ -3,6 +3,7 @@
 import { deleteCookie } from '@/action';
 import { loginState } from '@/shared/atoms/login';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useSetRecoilState } from 'recoil';
 import { twMerge } from 'tailwind-merge';
 
@@ -12,9 +13,11 @@ interface Props {
 
 const MoreSection = ({ className }: Props) => {
   const setLogin = useSetRecoilState(loginState);
+  const { push } = useRouter();
   const logout = async () => {
     await Promise.all([deleteCookie('accessToken'), deleteCookie('refreshToken')]);
     setLogin(false);
+    push('/mypage');
   };
 
   return (
@@ -22,11 +25,9 @@ const MoreSection = ({ className }: Props) => {
       <Link href="/mypage/withdraw">
         <button type="button">회원탈퇴</button>
       </Link>
-      <Link href="/mypage">
-        <button onClick={logout} type="button">
-          로그아웃
-        </button>
-      </Link>
+      <button onClick={logout} type="button">
+        로그아웃
+      </button>
     </div>
   );
 };
