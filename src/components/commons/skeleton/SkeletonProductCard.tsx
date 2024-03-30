095e554dccb89cb3686e0f0c968e0a024a5d0ed3
Wlfjd <leejiwon813@naver.com>
2024-03-30 23:02:07 +0900

    🐛 Fix :  skeleton ui grid 동적 클래스 방식 변경

--- src/components/commons/skeleton/SkeletonProductCard.tsx
+++ src/components/commons/skeleton/SkeletonProductCard.tsx
@@ -1,6 +1,6 @@
 export const SkeletonProductCard = () => {
   return (
-    <div className="w-[48%] animate-pulse">
+    <div className="w-full animate-pulse">
       <div className="w-full aspect-square bg-gray-300 rounded"></div>
       <div className="mt-2 h-4 bg-gray-300 rounded w-1/4"></div>
       <div className="mt-1 h-4 bg-gray-300 rounded"></div>
--- src/components/commons/skeleton/SkeletonProductList.tsx
+++ src/components/commons/skeleton/SkeletonProductList.tsx
@@ -5,10 +5,28 @@ interface SkeletonProductListProps {
   row?: number; // 행 개수
   col?: number; // 열 개수
 }
+type GridType = {
+  [key: string]: string;
+};
+
+const GRID_ROWS: GridType = {
+  1: 'grid-rows-1',
+  2: 'grid-rows-2',
+  3: 'grid-rows-3'
+};
+
+const GRID_COLS: GridType = {
+  1: 'grid-cols-1',
+  2: 'grid-cols-2',
+  3: 'grid-cols-3'
+};
 
 export const SkeletonProductList = ({ row = 3, col = 2 }: SkeletonProductListProps) => {
   return (
-    <PaddingWrapper key={`row-${row}`} className="flex flex-wrap gap-x-[16px] gap-y-[16px] pb-1">
+    <PaddingWrapper
+      key={`row-${row}`}
+      className={`grid ${GRID_ROWS[row]} ${GRID_COLS[col]} gap-x-[16px] gap-y-[16px]`}
+    >
       {Array(row * col)
         .fill(0)
         .map((_, idx) => (
