f7f2256cd0df82e3716b9fe50583fb11582aae76
jijiseong <jiseong5036@gmail.com>
2024-03-31 18:01:36 +0900

    💄 Design:  wish detail paddingwrapper 추가

--- src/app/wishlist/detail/products/[folderId]/page.tsx
+++ src/app/wishlist/detail/products/[folderId]/page.tsx
@@ -1,12 +1,13 @@
 import WishFolderEditButtonSection from '@/blocks/wish/WishFolderEditButtonSection';
 import WishProductList from '@/blocks/wish/WishProductList';
+import PaddingWrapper from '@/components/commons/PaddingWrapper';
 
 const WishProductsDetail = async () => {
   return (
-    <>
+    <PaddingWrapper>
       <WishFolderEditButtonSection />
       <WishProductList />
-    </>
+    </PaddingWrapper>
   );
 };
 
