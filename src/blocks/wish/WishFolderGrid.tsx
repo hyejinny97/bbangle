5c2fa30a898a235a3c4e88bfa5ec0153446e5d33
jijiseong <jiseong5036@gmail.com>
2024-03-31 16:05:20 +0900

    ✨ Feat :  조회 api 연동

--- src/blocks/wish/WishFolderGrid.tsx
+++ src/blocks/wish/WishFolderGrid.tsx
@@ -1,23 +1,12 @@
+import getWishList from '@/domains/user/queries/getWishList';
 import WishFolder from '@/domains/wish/components/WishFolder';
-import { WishFolderType } from '@/domains/wish/types/wishFolder';
-
-const MOCK_DATA: WishFolderType[] = [
-  {
-    folderId: 1,
-    title: 'mock data 제목입니다',
-    count: 1,
-    productImages: []
-  }
-];
 
 const WishFolderGrid = async () => {
-  // const res = await fetch('/wishLists');
-  // if (!res.ok) throw new Error('위시 리스트 조회 오류');
-  // const wishList: WishFolderType[] = await res.json();
+  const wishList = await getWishList();
 
   return (
     <div className="grid gap-[16px] grid-cols-2">
-      {MOCK_DATA.map(({ folderId, title, count, productImages }) => (
+      {wishList.map(({ folderId, title, count, productImages }) => (
         <WishFolder
           key={folderId}
           id={folderId}
---
+++ src/domains/user/queries/getWishList.ts
@@ -0,0 +1,12 @@
+import { WishFolderType } from '@/domains/wish/types/wishFolder';
+import fetchExtend from '@/shared/utils/api';
+
+const getWishList = async () => {
+  const res = await fetchExtend.get('/wishLists');
+  if (!res.ok) throw new Error('위시 리스트 조회 오류');
+  const wishList: WishFolderType[] = await res.json();
+
+  return wishList;
+};
+
+export default getWishList;
