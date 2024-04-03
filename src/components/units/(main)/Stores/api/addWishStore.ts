e47d5dfbfc3b51747906e0edd2b3ec0c34bda20c
hyejin <yhj970102@gmail.com>
2024-04-03 23:42:39 +0900

    ➖ Remove: 전체페이지(Store)에서 불필요한 파일 삭제

--- src/components/units/(main)/Stores/api/addWishStore.ts
+++
@@ -1,7 +0,0 @@
-import API from '@/api';
-import { WishStoreListReturn } from '../../types';
-
-export const addWishStore = async (storeId: number) => {
-  const data: WishStoreListReturn = await API.post(`/likes/store/${String(storeId)}`);
-  return data;
-};
