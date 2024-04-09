0b83d15c261c1f3d627e32b3554026b0fc9c9fab
hyejin <yhj970102@gmail.com>
2024-04-09 16:57:40 +0900

    ✨ Feat : flarelane SDK 삽입 및 초기화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import PopupContainer from '@/components/commons/PopupContainer';
 import ModalContainer from '@/components/commons/ModalContainer';
 import ToastContainer from '@/components/commons/ToastContainer';
 import KaKaoChatScript from '@/components/commons/KaKaoChatScript';
+import FlareLaneScript from '@/global/FlareLaneScript';
 
 const pretendard = localFont({
   src: '../commons/assets/PretendardVariable.woff2'
@@ -19,19 +20,20 @@ export const metadata: Metadata = {
     '빵그리의 오븐은 건강을 소중히 여기는 이들에게 새로운 디저트 경험을 선사하고, 건강과 맛을 모두 충족시킬 수 있는 특별한 공간을 제공합니다.'
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html>
-      <body className={pretendard.className}>
-        <RootLayoutProvider>
-          <ModalContainer />
-          <PopupContainer />
-          <ToastContainer />
-          <div className="sm:w-[600px] w-full m-auto pb-24 min-h-screen relative">{children}</div>
-          <Footer />
-        </RootLayoutProvider>
-        <KaKaoChatScript />
-      </body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: { children: ReactNode }) => (
+  <html lang="ko">
+    <body className={pretendard.className}>
+      <RootLayoutProvider>
+        <ModalContainer />
+        <PopupContainer />
+        <ToastContainer />
+        <div className="sm:w-[600px] w-full m-auto pb-24 min-h-screen relative">{children}</div>
+        <Footer />
+      </RootLayoutProvider>
+      <KaKaoChatScript />
+      <FlareLaneScript />
+    </body>
+  </html>
+);
+
+export default RootLayout;
