213c4cee2d5d0d39f22b6272088db737abb86004
jijiseong <jiseong5036@gmail.com>
2024-04-20 23:15:35 +0900

    🧪Test :  test coverage 세팅

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8'
+    }
   }
 });
