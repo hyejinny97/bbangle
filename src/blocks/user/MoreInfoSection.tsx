a99a2d551065e38d9c5f27d7d726fe04f00afeb0
jijiseong <jiseong5036@gmail.com>
2024-04-23 18:42:49 +0900

    ✨ Feat :  로그인시, 마이페이지 링크 추가

--- src/blocks/user/MoreInfoSection.tsx
+++ src/blocks/user/MoreInfoSection.tsx
@@ -1,35 +1,51 @@
 'use client';
 
 import Link from 'next/link';
+import { ReactNode } from 'react';
 import { chatKakaoChannel } from '@/domains/user/utils/chatKakaoChannel';
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
+import PATH from '@/shared/constants/path';
+import {
+  BellIcon,
+  DocIcon,
+  InquiryIcon,
+  LockIcon,
+  SettingIcon,
+  ThumbsUpIcon,
+  WriteIcon
+} from '@/domains/user/components/icons';
 import { useRecoilValue } from 'recoil';
 import { isLoggedinState } from '@/shared/atoms/login';
-import PATH from '@/shared/constants/path';
-import { BellIcon, DocIcon, InquiryIcon, LockIcon } from '@/domains/user/components/icons';
 
 interface MoreInfoItemProps {
-  icon: React.ReactNode;
+  icon: ReactNode;
   content: string;
 }
 
 const INFOS = [
   { href: PATH.notification, icon: <BellIcon />, content: '공지사항' },
   { href: PATH.serviceTerm, icon: <DocIcon />, content: '서비스 이용 약관' },
-  { href: PATH.privacyPolicy, icon: <LockIcon />, content: '개인정보 수집 및 이용' },
+  { href: PATH.privacyPolicy, icon: <LockIcon />, content: '개인정보 수집 및 이용' }
+];
+
+const LOGGEDIN_INFOS = [
+  { href: PATH.notification, icon: <BellIcon />, content: '공지사항' },
+  { href: '#', icon: <WriteIcon />, content: '내가 작성한 리뷰' },
+  { href: '#', icon: <ThumbsUpIcon />, content: '빵겟팅/재입고 알림 관리' },
+  { href: '#', icon: <SettingIcon />, content: '상품 추천하기' },
   { href: '#', icon: <InquiryIcon />, content: '문의하기' }
 ];
 
 const MoreInfoItem = ({ icon, content }: MoreInfoItemProps) => (
   <PaddingWrapper className="flex items-center gap-[8px] border-solid border-b-[1px] border-gray-100">
     {icon}
-    <p className="ml-[8px] text-[14px] font-medium">{content}</p>
+    <p className="text-[14px] font-medium">{content}</p>
   </PaddingWrapper>
 );
 
 const MoreInfoSection = () => {
   const isLoggedIn = useRecoilValue(isLoggedinState);
-  const information = isLoggedIn ? INFOS : INFOS.slice(0, INFOS.length - 1);
+  const information = isLoggedIn ? LOGGEDIN_INFOS : INFOS;
 
   return (
     <div>
--- src/domains/user/components/icons.tsx
+++ src/domains/user/components/icons.tsx
@@ -4,3 +4,6 @@ export { default as BellIcon } from '../assets/bell.svg';
 export { default as DocIcon } from '../assets/doc.svg';
 export { default as InquiryIcon } from '../assets/inquiry.svg';
 export { default as LockIcon } from '../assets/lock.svg';
+export { default as WriteIcon } from '../assets/write.svg';
+export { default as ThumbsUpIcon } from '../assets/thumbs-up.svg';
+export { default as SettingIcon } from '../assets/setting.svg';
