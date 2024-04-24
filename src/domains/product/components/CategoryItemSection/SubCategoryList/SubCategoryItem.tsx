3c4dbd008e71a77634da3c34d2035fb3a40bf341
Wlfjd <leejiwon813@naver.com>
2024-04-24 09:40:47 +0900

    💄 Design:  hover 스타일 수정

--- src/domains/product/components/CategoryItemSection/SubCategoryList/SubCategoryItem.tsx
+++ src/domains/product/components/CategoryItemSection/SubCategoryList/SubCategoryItem.tsx
@@ -7,7 +7,7 @@ interface MiddleCategoryItemProps {
 const SubCategoryItem = ({ item }: MiddleCategoryItemProps) => (
   <button
     type="button"
-    className="flex p-[16px] gap-[6px] bg-redGray-30 items-center w-full border-solid border-r-[0.5px] border-b-[0.5px] border-gray-200"
+    className="flex p-[16px] gap-[6px] bg-redGray-30 items-center w-full border-solid border-r-[0.5px] border-b-[0.5px] border-gray-200 hover:bg-gray-200"
   >
     <div className="font-medium text-start text-gray-700 text-14 flex-grow">
       {item}
