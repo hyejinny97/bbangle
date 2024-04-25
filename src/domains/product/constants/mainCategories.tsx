2990e42a18c2bad7f4563bd522d6326a7a541f59
Wlfjd <leejiwon813@naver.com>
2024-04-25 19:55:24 +0900

    🐛 Fix : backArrowIcon 오류 해결

--- src/domains/product/constants/mainCategories.tsx
+++ src/domains/product/constants/mainCategories.tsx
@@ -1,17 +1,23 @@
-import { AllIcon, BreadIcon, CookieIcon } from '../assets/category/icons';
+import ProductCategoryIcons from '@/shared/components/icons/ProductCategoryIcons';
 
 export const MAIN_CATEGORIES_TYPE = [
-  { id: 0, icon: <AllIcon />, title: '전체', hasMoreCategory: false, subCategories: [] },
+  {
+    id: 0,
+    icon: <ProductCategoryIcons type="all" />,
+    title: '전체',
+    hasMoreCategory: false,
+    subCategories: []
+  },
   {
     id: 1,
-    icon: <BreadIcon />,
+    icon: <ProductCategoryIcons type="bread" />,
     title: '빵',
     hasMoreCategory: true,
     subCategories: ['전체보기', '식빵ㆍ모닝빵', '베이글ㆍ도넛', '케이크ㆍ파이']
   },
   {
     id: 2,
-    icon: <CookieIcon />,
+    icon: <ProductCategoryIcons type="cookie" />,
     title: '간식/과자',
     hasMoreCategory: true,
     subCategories: [
--- src/shared/components/icons/ProductCategoryIcons.tsx
+++ src/shared/components/icons/ProductCategoryIcons.tsx
@@ -1,17 +1,20 @@
+import YogurtIcon from '@public/assets/icons/categories/all-icon.svg';
 import BreadIcon from '@public/assets/icons/categories/bread-icon.svg';
 import CakeIcon from '@public/assets/icons/categories/cake-icon.svg';
 import CookieIcon from '@public/assets/icons/categories/cookie-icon.svg';
 import EtcIcon from '@public/assets/icons/categories/etc-icon.svg';
 import JamIcon from '@public/assets/icons/categories/jam-icon.svg';
 import TartIcon from '@public/assets/icons/categories/tart-icon.svg';
-import YogurtIcon from '@public/assets/icons/categories/yogurt.svg';
+import AllIcon from '@public/assets/icons/categories/yogurt.svg';
 
 interface Props {
-  type: 'yogurt' | 'bread' | 'cake' | 'cookie' | 'etc' | 'jam' | 'tart';
+  type: 'all' | 'yogurt' | 'bread' | 'cake' | 'cookie' | 'etc' | 'jam' | 'tart';
 }
 
 const ProductCategoryIcons = ({ type }: Props) => {
   switch (type) {
+    case 'all':
+      return <AllIcon />;
     case 'yogurt':
       return <YogurtIcon />;
     case 'bread':
