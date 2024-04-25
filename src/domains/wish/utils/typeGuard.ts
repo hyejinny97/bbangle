a231a2c93559b30ddf626f206bb4b5ea8653ab28
jijiseong <jiseong5036@gmail.com>
2024-04-26 07:33:06 +0900

    ♻️ Refactor :  lint warning 제거

--- src/domains/wish/utils/typeGuard.ts
+++ src/domains/wish/utils/typeGuard.ts
@@ -1,8 +1,7 @@
 import { SortOption } from '../atoms/wishProducts';
 import { SORT_OPTIONS } from '../constants';
 
-export const isSortOptionType = (sortOption: string): sortOption is SortOption => {
-  return SORT_OPTIONS.includes(sortOption as SortOption);
-};
+export const isSortOptionType = (sortOption: string): sortOption is SortOption =>
+  SORT_OPTIONS.includes(sortOption as SortOption);
 
 export default isSortOptionType;
