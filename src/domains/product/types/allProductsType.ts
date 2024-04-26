b402ddfe21d644fbe6859f7927410985aa163ab8
Wlfjd <leejiwon813@naver.com>
2024-04-26 15:10:57 +0900

    ♻️ Refactor : 상품조회 api 리팩토링

--- src/domains/product/types/allProductsType.ts
+++ src/domains/product/types/allProductsType.ts
@@ -1,14 +1,9 @@
 import { IProductType } from './productType';
 
 export interface IAllProductsType {
-  success: boolean;
-  code: number;
-  message: string;
-  result: {
-    content: IProductType[];
-    requestCursor: number;
-    hasNext: boolean;
-    boardCount: number;
-    storeCount: number;
-  };
+  content: IProductType[];
+  requestCursor: number;
+  hasNext: boolean;
+  boardCount: number;
+  storeCount: number;
 }
