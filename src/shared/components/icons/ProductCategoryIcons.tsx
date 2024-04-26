a574cadeef7a99d0b50a438983c8f28e52ae7d98
Wlfjd <leejiwon813@naver.com>
2024-04-26 21:12:07 +0900

    🐛 Fix :  all-icon, yogurt 변경

--- src/shared/components/icons/ProductCategoryIcons.tsx
+++ src/shared/components/icons/ProductCategoryIcons.tsx
@@ -1,11 +1,11 @@
-import YogurtIcon from '@public/assets/icons/categories/all-icon.svg';
+import AllIcon from '@public/assets/icons/categories/all-icon.svg';
 import BreadIcon from '@public/assets/icons/categories/bread-icon.svg';
 import CakeIcon from '@public/assets/icons/categories/cake-icon.svg';
 import CookieIcon from '@public/assets/icons/categories/cookie-icon.svg';
 import EtcIcon from '@public/assets/icons/categories/etc-icon.svg';
 import JamIcon from '@public/assets/icons/categories/jam-icon.svg';
 import TartIcon from '@public/assets/icons/categories/tart-icon.svg';
-import AllIcon from '@public/assets/icons/categories/yogurt.svg';
+import YogurtIcon from '@public/assets/icons/categories/yogurt.svg';
 
 interface Props {
   shape: 'all' | 'yogurt' | 'bread' | 'cake' | 'cookie' | 'etc' | 'jam' | 'tart';
