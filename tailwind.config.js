3bac6a21689fe4136ac77721e5be002559181995
JiSeong Park <jiseong5036@gmail.com>
2024-05-06 06:33:00 +0900

    🐛 Fix :  typo 없는 속성 추가 (#328)

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,58 +74,109 @@ module.exports = {
   plugins: [
     require('tailwind-scrollbar-hide'), // 이미 존재하는 플러그인
     function ({ addUtilities }) {
-      const newUtilities = {
+      const typoUtilities = {
         '.typo-heading-20-semibold': {
           'font-size': '20px',
-          'font-weight': 600
+          'font-weight': 600,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-title-16-regular': {
           'font-size': '16px',
-          'font-weight': 400
+          'font-weight': 400,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-title-16-medium': {
           'font-size': '16px',
-          'font-weight': 500
+          'font-weight': 500,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
+        },
+        '.typo-title-16-semibold': {
+          'font-size': '16px',
+          'font-weight': 600,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
+        },
+
+        '.typo-title-14-regular': {
+          'font-size': '14px',
+          'font-weight': 400,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
+        },
+        '.typo-title-14-medium': {
+          'font-size': '14px',
+          'font-weight': 500,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-title-14-semibold': {
           'font-size': '14px',
-          'font-weight': 600
+          'font-weight': 600,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-title-14-bold': {
           'font-size': '14px',
-          'font-weight': 700
+          'font-weight': 700,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
+
         '.typo-body-12-regular': {
           'font-size': '12px',
-          'font-weight': 400
+          'font-weight': 400,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-12-medium': {
           'font-size': '12px',
-          'font-weight': 500
+          'font-weight': 500,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-12-semibold': {
           'font-size': '12px',
-          'font-weight': 600
+          'font-weight': 600,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-12-bold': {
           'font-size': '12px',
-          'font-weight': 700
+          'font-weight': 700,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-12-regular-underline': {
           'font-size': '12px',
           'font-weight': 500,
-          'text-decoration': 'underline'
+          'text-decoration': 'underline',
+          'line-height': '150%',
+          'letter-spacing': '-2em'
+        },
+
+        '.typo-body-11-regular': {
+          'font-size': '11px',
+          'font-weight': 500,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-11-semibold': {
           'font-size': '11px',
-          'font-weight': 600
+          'font-weight': 600,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         },
         '.typo-body-11-bold': {
           'font-size': '11px',
-          'font-weight': 700
+          'font-weight': 700,
+          'line-height': '150%',
+          'letter-spacing': '-2em'
         }
       };
-      addUtilities(newUtilities);
+      addUtilities(typoUtilities);
     }
   ]
 };
