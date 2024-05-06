412e9451b252161bdcc8004ef6bddd176fefa0f0
JIWON <leejiwon813@naver.com>
2024-05-07 00:48:48 +0900

    📖 Story : 5개의 스토리북 생성 (#331)

---
+++ src/stories/ProductAndStoreTab.stories.tsx
@@ -0,0 +1,17 @@
+import ProductAndStoreTab from '@/shared/components/ProductAndStoreTab';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ProductAndStoreTab> = {
+  component: ProductAndStoreTab,
+  title: 'ProductAndStoreTab'
+};
+export default meta;
+type Story = StoryObj<typeof ProductAndStoreTab>;
+
+export const Default: Story = {};
+export const hasCount: Story = {
+  args: {
+    productCount: 10,
+    storeCount: 88
+  }
+};
