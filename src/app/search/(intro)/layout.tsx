00586bd54ba021406dd42dba2abe2790d375a75e
HyeJin <yhj970102@gmail.com>
2024-05-08 23:34:23 +0900

    ✨ Feat : 인기 검색어 조회 API 에러 처리 (#343)

--- src/app/search/(intro)/layout.tsx
+++ src/app/search/(intro)/layout.tsx
@@ -1,22 +1,25 @@
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
-import RecentKeywordSection from '@/blocks/search/RecentKeywordSection';
-import PopularKeywordSection from '@/blocks/search/PopularKeywordSection';
 
-const SearchPage = () => (
+interface SearchIntroLayoutProps {
+  recentKeyword: React.ReactNode;
+  popularKeyword: React.ReactNode;
+}
+
+const SearchIntroLayout = ({ recentKeyword, popularKeyword }: SearchIntroLayoutProps) => (
   <>
     <PaddingWrapper className="flex flex-col gap-[10px]">
       <div className="text-gray-500 text-14 font-semibold leading-150 tracking-tight-2">
         최근 검색어
       </div>
-      <RecentKeywordSection />
+      {recentKeyword}
     </PaddingWrapper>
     <PaddingWrapper className="flex flex-col gap-[10px]">
       <div className="text-gray-500 text-14 font-semibold leading-150 tracking-tight-2">
         인기 검색어
       </div>
-      <PopularKeywordSection />
+      {popularKeyword}
     </PaddingWrapper>
   </>
 );
 
-export default SearchPage;
+export default SearchIntroLayout;
