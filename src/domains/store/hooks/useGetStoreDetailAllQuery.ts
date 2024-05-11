83b9872852501ca5e5c7b0e1d3a3b3b0909624f4
Wlfjd <leejiwon813@naver.com>
2024-05-11 23:21:06 +0900

    ♻️ Refactor :  스토어 api 리팩토링

--- src/domains/store/hooks/useGetStoreDetailAllQuery.ts
+++ src/domains/store/hooks/useGetStoreDetailAllQuery.ts
@@ -1,12 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { ResultResponse } from '@/shared/types/response';
 import fetchExtend from '@/shared/utils/api';
+import { throwApiError } from '@/shared/utils/error';
+import { useQuery } from '@tanstack/react-query';
+
 import { IStoreDetailAllType } from '../types/storeDetailType';
 
 const getStoreDetailAll = async (id: number) => {
   const res = await fetchExtend.get(`/stores/${id}/boards/all?page=0`);
   if (!res.ok) throw new Error('상품 상세 조회 실패');
-  const data: IStoreDetailAllType = await res.json();
-  return data;
+  const { success, result, code, message }: ResultResponse<IStoreDetailAllType> = await res.json();
+  if (!res.ok || !success) {
+    throwApiError({ code, message });
+  }
+  return result;
 };
 
 export const useGetStoreDetailAllQuery = (id: number) =>
