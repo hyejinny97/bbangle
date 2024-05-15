e0778e9b0597ebdc1cefea4b56532fba4bbad468
JIWON <leejiwon813@naver.com>
2024-05-16 07:59:05 +0900

    ♻️ Refactor :  스토어 상세 api 리팩토링 (#361)
    Co-authored-by: jijiseong <jiseong5036@gmail.com>

--- src/domains/store/hooks/useGetStoreDetailQuery.ts
+++ src/domains/store/hooks/useGetStoreDetailQuery.ts
@@ -1,12 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
+import { ResultResponse } from '@/shared/types/response';
 import fetchExtend from '@/shared/utils/api';
+import { throwApiError } from '@/shared/utils/error';
+import { useQuery } from '@tanstack/react-query';
+
 import { IStoreDetailType } from '../types/storeDetailType';
 
 const getStoreDetail = async (id: number): Promise<IStoreDetailType> => {
   const res = await fetchExtend.get(`/stores/${id}`);
-  if (!res.ok) throw new Error('상품 상세 조회 실패');
-  const data: IStoreDetailType = await res.json();
-  return data;
+  const { success, result, code, message }: ResultResponse<IStoreDetailType> = await res.json();
+  if (!res.ok || !success) {
+    throwApiError({ code, message });
+  }
+  return result;
 };
 
 export const useGetStoreDetailQuery = (id: number) =>
