06a4a4948dc0b67318083a6ce8afe8822bc870c0
hyejin <yhj970102@gmail.com>
2024-09-08 14:29:49 +0900

    ✨ Feat : 리뷰 상세 페이지에서 리뷰 삭제 시 뒤로 가기

--- src/domains/review/components/Review/KebabMenu.tsx
+++ src/domains/review/components/Review/KebabMenu.tsx
@@ -4,7 +4,7 @@ import Dropdown from '@/shared/components/Dropdown';
 import { KebabIcon } from '@/shared/components/icons';
 import PATH from '@/shared/constants/path';
 import { attachRedirectUrl } from '@/shared/utils/attachRedirectUrl';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { ReviewType } from '../../types/review';
 import useDeleteReviewMutation from '../../queries/useDeleteReviewMutation';
 
@@ -14,8 +14,10 @@ interface Props {
 }
 
 const KebabMenu = ({ reviewId, boardId }: Props) => {
-  const { push } = useRouter();
+  const { push, back } = useRouter();
   const { mutate: deleteMutate } = useDeleteReviewMutation(boardId);
+  const pathname = usePathname();
+  const isReviewDetailPage = pathname === PATH.reviewDetail({ productId: boardId, reviewId });
 
   const onModifyClick = () => {
     push(
@@ -28,6 +30,7 @@ const KebabMenu = ({ reviewId, boardId }: Props) => {
 
   const onDeleteClick = () => {
     deleteMutate(reviewId);
+    if (isReviewDetailPage) back();
   };
 
   return (
