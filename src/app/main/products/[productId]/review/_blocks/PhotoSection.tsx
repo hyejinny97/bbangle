d96dabd960c03414d81680119da3b2246217f27b
hyejin <yhj970102@gmail.com>
2024-09-19 16:31:21 +0900

    ♻️ Refactor :  `PhotoSection` 리팩토링

--- src/app/main/products/[productId]/review/_blocks/PhotoSection.tsx
+++ src/app/main/products/[productId]/review/_blocks/PhotoSection.tsx
@@ -15,46 +15,40 @@ interface Props {
 
 const PhotoSection = ({ photos, productId }: Props) => {
   const { openFullScreenModal } = useFullScreenModal();
+  const fourthImage = photos[3];
 
   return (
     <div className="grid grid-cols-4 gap-[4px]">
-      {photos.slice(0, 4).map(({ id, url }, idx) => {
-        const fourthImage = idx === 3;
-        if (fourthImage) {
-          return (
-            <Link
-              key={id}
-              href={PATH.reviewPhotos(productId)}
-              className="relative after:content-['+더보기'] after:flex-center after:absolute after:inset-0 after:bg-black/50 after:size-full after:text-white after:rounded-[6px]"
-            >
-              <Image
-                src={url}
-                alt="best review image"
-                width={80}
-                height={80}
-                blurDataURL={BLUR_DATA_URL}
-                className="w-full aspect-square object-cover border-solid border-[1px] border-gray-300 rounded-[6px]"
-              />
-            </Link>
-          );
-        }
-        return (
+      {photos.slice(0, 3).map(({ id, url }, idx) => (
+        <Image
+          key={id}
+          src={url}
+          alt="best review image"
+          width={80}
+          height={80}
+          blurDataURL={BLUR_DATA_URL}
+          onClick={() =>
+            openFullScreenModal(<PhotoSlideModal photos={photos.slice(0, 4)} initSlideIdx={idx} />)
+          }
+          className="w-full aspect-square object-cover border-solid border-[1px] border-gray-300 rounded-[6px] cursor-pointer"
+        />
+      ))}
+      {fourthImage && (
+        <Link
+          key={fourthImage.id}
+          href={PATH.reviewPhotos(productId)}
+          className="relative after:content-['+더보기'] after:flex-center after:absolute after:inset-0 after:bg-black/50 after:size-full after:text-white after:rounded-[6px]"
+        >
           <Image
-            key={id}
-            src={url}
+            src={fourthImage.url}
             alt="best review image"
             width={80}
             height={80}
             blurDataURL={BLUR_DATA_URL}
-            onClick={() =>
-              openFullScreenModal(
-                <PhotoSlideModal photos={photos.slice(0, 4)} initSlideIdx={idx} />
-              )
-            }
-            className="w-full aspect-square object-cover border-solid border-[1px] border-gray-300 rounded-[6px] cursor-pointer"
+            className="w-full aspect-square object-cover border-solid border-[1px] border-gray-300 rounded-[6px]"
           />
-        );
-      })}
+        </Link>
+      )}
     </div>
   );
 };
