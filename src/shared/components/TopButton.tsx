48430d230dcd9e1bf4e92e74e6f66c686d31d684
hyejin <yhj970102@gmail.com>
2024-10-03 16:54:45 +0900

    💄 Design: footer 높이 + 16px에 탑버튼이 위치하도록 수정

--- src/shared/components/TopButton.tsx
+++ src/shared/components/TopButton.tsx
@@ -1,17 +1,26 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { ELEMENT_ID } from '@/shared/constants/elementId';
+import { cn } from '@/shared/utils/cn';
 import ArrowIcons from '@/shared/components/icons/ArrowIcons';
 
 const TopButton = () => {
-  const [showButton, setShowButton] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [translateY, setTranslateY] = useState(0);
+
+  useEffect(() => {
+    const footer = document.getElementById(ELEMENT_ID.footer);
+    if (!footer) return;
+    const DISTANCE_FROM_FOOTER = 16;
+    setTranslateY(-footer.clientHeight - DISTANCE_FROM_FOOTER);
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) setShowButton(true);
-      else setShowButton(false);
+      if (window.scrollY > 0) setScrolled(true);
+      else setScrolled(false);
     };
-
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -25,13 +34,17 @@ const TopButton = () => {
     });
   };
 
-  if (!showButton) return null;
-
   return (
     <button
       type="button"
       aria-label="top button"
-      className="fixed z-bottomButton bottom-[104px] right-[16px] sm:right-[calc(50%-300px+16px)] w-[46px] h-[46px] flex-center rounded-full bg-white shadow-[0_0_4px_0_rgba(0,0,0,0.16)]"
+      className={cn(
+        'fixed z-topButton bottom-0 right-[16px] sm:right-[calc(50%-300px+16px)] w-[46px] h-[46px] flex-center rounded-full bg-white shadow-[0_0_4px_0_rgba(0,0,0,0.16)] invisible',
+        scrolled && translateY !== 0 && 'visible'
+      )}
+      style={{
+        transform: `translateY(${translateY}px)`
+      }}
       onClick={handleClick}
     >
       <ArrowIcons shape="top" />
